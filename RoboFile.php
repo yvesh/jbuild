498f0c32d97b9e96b841faec307c268e6b41d712
nibra <nbraczek@bsds.de>
2015-10-31 15:56:44 +0100

    Reverted removal of traits

--- RoboFile.php
+++ RoboFile.php
@@ -1,22 +1,89 @@
 <?php
 /**
- * Project Robo File
+ * @package    JBuild
+ * @author     Yves Hoppe <yves@compojoom.com>
+ * @date       20.09.15
  *
- * Extend this file according to your needs.
+ * @copyright  Copyright (C) 2008 - 2015 Yves Hoppe - compojoom.com . All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
  */
+
+namespace JBuild;
+
+use JBuild\Tasks\loadTasks;
 
 if (!defined('JPATH_BASE'))
 {
-    define('JPATH_BASE', __DIR__);
+	define('JPATH_BASE', __DIR__);
 }
 
 // PSR-4 Autoload by composer
 require_once JPATH_BASE . '/vendor/autoload.php';
 
 /**
- * Class RoboFile
+ * Standalone RoboFile for JBuild
+ *
+ * @since  5.3
  */
-class RoboFile extends \JBuild\RoboFile
+class RoboFile extends \Robo\Tasks
 {
+	use loadTasks;
 
+	/**
+	 * Initialize Robo
+	 */
+	public function __construct()
+	{
+		$this->stopOnFail(true);
+	}
+
+	/**
+	 * Map into Joomla installation.
+	 *
+	 * @param   String   $target    The target joomla instance
+	 *
+	 * @return  void
+	 */
+	public function map($target)
+	{
+		(new Tasks\Map($target))->run();
+	}
+
+	/**
+	 * Build the joomla extension package
+	 *
+	 * @param   array  $params  Additional params
+	 *
+	 * @return  void
+	 */
+	public function build($params = ['dev' => false])
+	{
+		(new Tasks\Build($params))->run();
+	}
+
+	/**
+	 * Generate an extension skeleton - not implemented yet
+	 *
+	 * @param   array  $extensions  Extensions to build (com_xy, mod_xy)
+	 *
+	 * @return  void
+	 */
+	public function generate($extensions)
+	{
+		(new Tasks\Generate($extensions))->run();
+	}
+
+	public function metrics($subtask = null)
+	{
+		if ($subtask == 'codestyle')
+		{
+			$task = new Tasks\Metrics\CodeStyle();
+		}
+		else
+		{
+			throw new \InvalidArgumentException;
+		}
+
+		$task->run();
+	}
 }
---
+++ src/Tasks/loadTasks.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package    JBuild
+ * @author     Yves Hoppe <yves@compojoom.com>
+ * @date       20.09.15
+ *
+ * @copyright  Copyright (C) 2008 - 2015 Yves Hoppe - compojoom.com . All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace JBuild\Tasks;
+
+trait loadTasks
+{
+    /**
+     * Map Task
+     *
+     * @param   String  $target  - The target directory
+     *
+     * @return  Map
+     */
+    protected function taskMap($target)
+    {
+        return new Map($target);
+    }
+
+    /**
+     * The build task
+     *
+     * @param   array  $params  - Opt params
+     *
+     * @return  Build
+     */
+    protected function taskBuild($params)
+    {
+        return new Build($params);
+    }
+
+    /**
+     * The generate task
+     *
+     * @param   array  $params  - Opt params
+     *
+     * @return  Build
+     */
+    protected function taskGenerate($params)
+    {
+        return new Generate($params);
+    }
+}
